52dca80cb4e95b4505c69b341b1bcf5bcfe4cdd9
osdodo <osdodo@qq.com>
2023-12-25 12:32:01 +0800

    fix feedback

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,10 @@ const HomePage = observer(() => {
           <VoiceWaves
             onText={(data, isTranscribing) => {
               const { transcription, temperature, language, no_speech_prob } = data;
-              const text = `<p style=${getStyle(temperature)}>${transcription} 【${language}】${no_speech_prob > 0.1 ? '【噪音】' : ''}</p>`;
+              if (!transcription || !language || no_speech_prob === undefined) {
+                return;
+              }
+              const text = `<p style=${getStyle(temperature)}>${transcription}【${language}】【no_speech_prob: ${no_speech_prob.toFixed(2)}】</p>`;
               if (isTranscribing) {
                 typewriterRef.current.typeString(text).start();
               } else {
