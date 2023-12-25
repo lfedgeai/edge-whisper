c9c16e222cbbd9f227a79958a199cf9396b553f5
osdodo <osdodo@qq.com>
2023-12-25 22:21:14 +0800

    update volumeThreshold

--- src/components/VoiceWaves/index.tsx
+++ src/components/VoiceWaves/index.tsx
@@ -152,7 +152,7 @@ export const VoiceWaves = observer(
       const dataArray = new Uint8Array(bufferLength);
 
       // 设置判断声音大小的阈值
-      const volumeThreshold = 140;
+      const volumeThreshold = 150;
 
       clearInterval(checkVolumeRef.current);
       // 定时检测声音大小
